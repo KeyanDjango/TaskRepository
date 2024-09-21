e762036cc87d0ed15d49347837208a5ab4069bdb
KeyanDjango <karthick.mendix@gmail.com>
2024-09-21 13:07:12 +0530

    changes made in task counter

--- src/TaskPage/Counter.jsx
+++ src/TaskPage/Counter.jsx
@@ -1,11 +1,21 @@
 import React, { useState, useRef } from 'react';
-
+import '../TaskPage/Task.css'
 
 export default function Counter() {
 
     const [count, setCount] = useState(0);
     const [txt, setTxt] = useState('');
     const txtfocus = useRef();
+    const [no1,setNo1] = useState(''); 
+    const [no2,setNo2] = useState('');
+    const [res,setRes] = useState(0);
+
+    const handleClickRes = () =>{
+        setRes(parseInt(no1) + parseInt(no2));
+        setNo1('');
+        setNo2('');
+        
+    }
 
     const handleClickFocus = () => {
         txtfocus.current.focus();
@@ -52,14 +62,35 @@ export default function Counter() {
                         <br />
                         <h4>TextInput & Replicate Below</h4> <br />
 
-                        <input ref={txtfocus} onChange={(e) => setTxt(e.target.value)} type="text" style={{ width: 400, height: 40, padding: 10, fontSize: 30 }} autoCapitalize='off' autoCorrect='off' /><br /> <br /><br />
+                        <input value={txt} ref={txtfocus} onChange={(e) => setTxt(e.target.value)} type="text" style={{ width: 400, height: 35, padding: 15, fontSize: 30, borderRadius: 30 }} autoCapitalize='off' autoCorrect='off' /><br /> <br /><br />
                         <p> {txt.length > 0 ? txt : 'TYPE SOMETHING IN TEXT BOX'} </p><br />
                         {txt.length}<br /><br />
 
-                        <button onClick={() => handleClickFocus()} style={{ width: 250, height: 40 }}>Focus</button>
+                        <button onClick={() => handleClickFocus()} style={{ color: 'black', width: 250, height: 40, fontWeight: 'bold' }}>Focus</button><br /> <br />
 
+                        <button  onClick={() => setTxt('')}  style={{width:300,height:40,borderRadius:10,border:'0.5px solid black'}}>CLEAR</button>
 
                     </center>
+
+                    <br /> <br /> <hr /><br /> <br /> 
+
+
+                    
+                        <div className="task-addtwo">
+
+                         <center>
+                          <input  value={no1} placeholder="Enter Number1" onChange={(v) => setNo1(v.target.value)}  type="txt" style={{ width: 400, height: 35, padding: 15, fontSize: 30, borderRadius: 30 }} /><br/> <br/>
+                          <input value={no2} onChange={(e) => setNo2(e.target.value)} placeholder="Enter Number2" type="txt" style={{ width: 400, height: 35, padding: 15, fontSize: 30, borderRadius: 30 }}/> <br/>  <br/>
+                          <button  onClick={() => handleClickRes()}  style={{width:300,height:40,borderRadius:10,border:'0.5px solid black'}}>ADD</button><br /> <br />
+                          <p>RESULT {res}</p>
+                          <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> 
+                          
+                         </center>
+                           
+                        </div>
+                    
+
+
 
                 </div>
 
